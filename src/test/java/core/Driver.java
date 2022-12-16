99c1972a959f73a19db3cdbb2e9d4f8e4a9d60b9
Jardeson Santos <jardounifanor@gmail.com>
2022-12-15 23:26:53 -0300

    add accessibility testing axe-selenium library on the project

--- src/test/java/core/Driver.java
+++ src/test/java/core/Driver.java
@@ -1,16 +1,19 @@
 package core;
 
+import com.deque.axe.AXE;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -18,6 +21,12 @@ public class Driver {
         O pacote core com as classes Driver e Browser são responsáveis por todas as configurações
         do navegador
      */
+    private static final URL scriptUrl = Driver.class.getResource("/axe.min.js");
+
+    public static URL getScriptUrl() {
+        return scriptUrl;
+    }
+
     private static WebDriver driver; //get
     private static WebDriverWait wait;
     private static String nameScenario; //setter
@@ -36,8 +45,18 @@ public class Driver {
                 startFirefox();
                 break;
         }
-        wait = new WebDriverWait(driver, 30);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        JSONObject responseJson = new AXE.Builder(Driver.getDriver(),Driver.getScriptUrl()).analyze();
+        JSONArray violations = responseJson.getJSONArray("violations");
+        if (violations.length()==0){
+            System.out.println("No errors");
+        } else {
+            AXE.writeResults("AllyTestAutomationPraticeSite", responseJson);
+            System.out.println(AXE.report(violations));
+        }
+
     }
 
     /*
@@ -105,6 +124,8 @@ public class Driver {
         chromeOptions.addArguments("--start-maximized");
         chromeOptions.addArguments("--incognito");
         driver = new ChromeDriver(chromeOptions);
+
+
     }
 
 
